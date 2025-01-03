538777c8bb7cc4a16fad834a8927915d232f0810
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2025-01-02 18:08:09 -0600

    Final Commit

--- API/app.py
+++ API/app.py
@@ -1,10 +1,10 @@
 import requests
 
 # URL de la API
-url = "https://41d8-34-125-79-249.ngrok-free.app/predict/"  # Ajusta el puerto si es necesario
+url = "http://127.0.0.1:8001/predict/"  # Ajusta el puerto si es necesario
 
 # Ruta de la imagen que deseas enviar
-image_path = "../Purpura.jpg"
+image_path = "../Chinche.jpg"
 
 # Abrir la imagen en modo binario
 with open(image_path, "rb") as image_file:
